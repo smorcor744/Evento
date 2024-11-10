0d4cdd5258b5aad049578f6e630fc98bb1d85a2e
Sergio <smorcor744a@g.educaand.es>
2024-11-10 18:52:16 +0100

    Terminado

--- app/src/main/java/com/example/eventoo/MainActivity.kt
+++ app/src/main/java/com/example/eventoo/MainActivity.kt
@@ -3,24 +3,36 @@ package com.example.eventoo
 import android.graphics.Color
 import android.os.Bundle
 import android.widget.Button
-import android.widget.LinearLayout
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var layout: LinearLayout
-    private lateinit var boton: Button
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.button)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
 
-        layout = findViewById(R.id.layout)
-        boton = findViewById(R.id.boton)
+        val mainLayout = findViewById<androidx.constraintlayout.widget.ConstraintLayout>(R.id.button)
+        val changeColorButton = findViewById<Button>(R.id.boton)
 
-        boton.setOnClickListener {
-            layout.setBackgroundColor(Color.parseColor("#FFEB3B"))
+        changeColorButton.setOnClickListener {
+            val color = colorRandom()
+            mainLayout.setBackgroundColor(color)
         }
     }
-}
 
+    private fun colorRandom(): Int {
+        val red = Random.nextInt(256)
+        val green = Random.nextInt(256)
+        val blue = Random.nextInt(256)
+        return Color.rgb(red, green, blue)
+    }
+}
